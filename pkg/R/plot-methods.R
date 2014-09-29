e759028676e176050fc0ef57c0b1a8ecfea83755
lorenzo <lorenzo@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-09-29 22:32:51 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cogarch@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot-methods.R
+++
@@ -1,9 +0,0 @@
-setMethod("plot",
-          signature(x = "trajectories", y = "missing"),
-          function (x, y, ...) 
-          {
-            par(mfrow=c(2,1))
-            matplot(x@time, x@G, type="s", xlab="t", ylab="", xlim=c(0, max(x@time)), main="G", ...)
-            matplot(x@time, x@sigma, type="s", xlab="t", ylab="", xlim=c(0, max(x@time)), main="sigma", ...)
-          }
-)
