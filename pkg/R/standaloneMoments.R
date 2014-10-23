66e1b37b9940e4751c7d9b1a664734562cea3435
ebibbona <ebibbona@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-10-23 14:44:46 +0000

    momenti sistemati
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cogarch@22 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/standaloneMoments.R
+++ pkg/R/standaloneMoments.R
@@ -1,43 +1,103 @@
-EG2 <- function(h, theta, psi) .C("EG2", as.double(h), as.double(theta), as.double(psi), result=as.double(numeric(1)))$result
+EG2 <- function(h, cogprm, levy){
+	psi.aug<-setPsi(cogprm,levy)
+	psi<-psi.aug[-5]
+	theta<-c(cogprm@beta, cogprm@eta, psi.aug[5], cogprm@phi)
+	.C("EG2", as.double(h), as.double(theta), as.double(psi), result=as.double(numeric(1)))$result}
 
-EG2G2 <- function(delta1, h, theta, psi)
+EG2G2 <- function(delta1, h,cogprm, levy){
+	psi.aug<-setPsi(cogprm,levy)
+	psi<-psi.aug[-5]
+	theta<-c(cogprm@beta, cogprm@eta, psi.aug[5], cogprm@phi)
 .C("EG2G2", as.double(delta1), as.double(h), as.double(theta), as.double(psi), result=as.double(numeric(1)))$result
+}
 
-EG4 <- function(h, theta, psi)
+EG4 <- function(h, cogprm, levy){
+	psi.aug<-setPsi(cogprm,levy)
+	psi<-psi.aug[-5]
+	theta<-c(cogprm@beta, cogprm@eta, psi.aug[5], cogprm@phi)
 .C("EG4", as.double(h), as.double(theta), as.double(psi), result=as.double(numeric(1)))$result
+}
 
-EG2G2G2 <- function(delta1, delta2, h, theta, psi)
+EG2G2G2 <- function(delta1, delta2, h,cogprm, levy){
+	psi.aug<-setPsi(cogprm,levy)
+	psi<-psi.aug[-5]
+	theta<-c(cogprm@beta, cogprm@eta, psi.aug[5], cogprm@phi)
 .C("EG2G2G2", as.double(delta1), as.double(delta2), as.double(h), as.double(theta), as.double(psi), result=as.double(numeric(1)))$result
+}
 
-EG4G2 <- function(delta2, h, theta, psi)
+EG4G2 <- function(delta2, h, cogprm, levy){
+	psi.aug<-setPsi(cogprm,levy)
+	psi<-psi.aug[-5]
+	theta<-c(cogprm@beta, cogprm@eta, psi.aug[5], cogprm@phi)
 .C("EG4G2", as.double(delta2), as.double(h), as.double(theta), as.double(psi), result=as.double(numeric(1)))$result
+}
 
-EG2G4 <- function(delta1, h, theta, psi)
+EG2G4 <- function(delta1, h, cogprm, levy){
+	psi.aug<-setPsi(cogprm,levy)
+	psi<-psi.aug[-5]
+	theta<-c(cogprm@beta, cogprm@eta, psi.aug[5], cogprm@phi)
 .C("EG2G4", as.double(delta1), as.double(h), as.double(theta), as.double(psi), result=as.double(numeric(1)))$result
+}
 
-EG6 <- function(h, theta, psi)
+EG6 <- function(h, cogprm, levy){
+	psi.aug<-setPsi(cogprm,levy)
+	psi<-psi.aug[-5]
+	theta<-c(cogprm@beta, cogprm@eta, psi.aug[5], cogprm@phi)
 .C("EG6", as.double(h), as.double(theta), as.double(psi), result=as.double(numeric(1)))$result
+}
 
-EG2G2G2G2 <- function(delta1, delta2, delta3, h, theta, psi)
+EG2G2G2G2 <- function(delta1, delta2, delta3, h, cogprm, levy){
+	psi.aug<-setPsi(cogprm,levy)
+	psi<-psi.aug[-5]
+	theta<-c(cogprm@beta, cogprm@eta, psi.aug[5], cogprm@phi)
 .C("EG2G2G2G2", as.double(delta1), as.double(delta2), as.double(delta3), as.double(h), as.double(theta), as.double(psi), result=as.double(numeric(1)))$result
+}
 
-EG4G2G2 <- function(delta2, delta3, h, theta, psi)
+EG4G2G2 <- function(delta2, delta3, h, cogprm, levy){
+	psi.aug<-setPsi(cogprm,levy)
+	psi<-psi.aug[-5]
+	theta<-c(cogprm@beta, cogprm@eta, psi.aug[5], cogprm@phi)
 .C("EG4G2G2", as.double(delta2), as.double(delta3), as.double(h), as.double(theta), as.double(psi), result=as.double(numeric(1)))$result
+}
 
-EG2G4G2 <- function(delta1, delta3, h, theta, psi)
+EG2G4G2 <- function(delta1, delta3, h, cogprm, levy){
+	psi.aug<-setPsi(cogprm,levy)
+	psi<-psi.aug[-5]
+	theta<-c(cogprm@beta, cogprm@eta, psi.aug[5], cogprm@phi)
 .C("EG2G4G2", as.double(delta1), as.double(delta3), as.double(h), as.double(theta), as.double(psi), result=as.double(numeric(1)))$result
+}
 
-EG6G2 <- function(delta3, h, theta, psi)
+EG6G2 <- function(delta3, h, cogprm, levy){
+	psi.aug<-setPsi(cogprm,levy)
+	psi<-psi.aug[-5]
+	theta<-c(cogprm@beta, cogprm@eta, psi.aug[5], cogprm@phi)
 .C("EG6G2", as.double(delta3), as.double(h), as.double(theta), as.double(psi), result=as.double(numeric(1)))$result
+}
 
-EG2G2G4 <- function(delta1, delta2, h, theta, psi)
+EG2G2G4 <- function(delta1, delta2, h, cogprm, levy){
+	psi.aug<-setPsi(cogprm,levy)
+	psi<-psi.aug[-5]
+	theta<-c(cogprm@beta, cogprm@eta, psi.aug[5], cogprm@phi)
 .C("EG2G2G4", as.double(delta1), as.double(delta2), as.double(h), as.double(theta), as.double(psi), result=as.double(numeric(1)))$result
+}
 
-EG4G4 <- function(delta2, h, theta, psi)
+EG4G4 <- function(delta2, h, cogprm, levy){
+	psi.aug<-setPsi(cogprm,levy)
+	psi<-psi.aug[-5]
+	theta<-c(cogprm@beta, cogprm@eta, psi.aug[5], cogprm@phi)
 .C("EG4G4", as.double(delta2), as.double(h), as.double(theta), as.double(psi), result=as.double(numeric(1)))$result
+}
 
-EG2G6 <- function(delta1, h, theta, psi)
+EG2G6 <- function(delta1, h, cogprm, levy){
+	psi.aug<-setPsi(cogprm,levy)
+	psi<-psi.aug[-5]
+	theta<-c(cogprm@beta, cogprm@eta, psi.aug[5], cogprm@phi)
 .C("EG2G6", as.double(delta1), as.double(h), as.double(theta), as.double(psi), result=as.double(numeric(1)))$result
+}
 
-EG8 <- function(h, theta, psi)
+EG8 <- function(h, cogprm, levy){
+	psi.aug<-setPsi(cogprm,levy)
+	psi<-psi.aug[-5]
+	theta<-c(cogprm@beta, cogprm@eta, psi.aug[5], cogprm@phi)
 .C("EG8", as.double(h), as.double(theta), as.double(psi), result=as.double(numeric(1)))$result
+}
